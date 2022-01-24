6886481945a7878f95ca773ce5fa3f668e78fe06
Léia Ribeiro <leiaribeirot@gmail.com>
2022-01-23 21:31:41 -0400

    Requisito 3, getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const firsLastName = employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
+  return firsLastName;
 }
 
 module.exports = getEmployeeByName;

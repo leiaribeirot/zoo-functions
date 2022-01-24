d6527755218bdd28d32062582aec963f2e9aee2e
Léia Ribeiro <leiaribeirot@gmail.com>
2022-01-23 21:10:11 -0400

    Requisito 1, getSpeciesBYIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data'); // descontrução de especies
 
-function getSpeciesByIds(ids) {
-  // abrindo PR
+function getSpeciesByIds(...ids) {
+  // Mapa do ID's, comparar mapResult com findResult e verifica se igual.
+  const arrSpecies = ids.map((mapResult) => species
+    .find((findResult) => findResult.id === mapResult));
+  return arrSpecies;
 }
 
 module.exports = getSpeciesByIds;
